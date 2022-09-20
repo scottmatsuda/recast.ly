0063a66029d46658faa4276ef510689bcff20977
Scott Matsuda <smatsuda2014@gmail.com>
2022-09-19 17:34:52 -0700

    Completed videolist and videolistentry checkmarks

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,21 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ({videos}) => (
   <div className="video-list">
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
+    {/* {let videos = Array.from(props)} */}
+    {videos.map((video) => (
+      <VideoListEntry video={video} />
+    ))}
+    {/* <div><h5><em>{console.log('props', props)}</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
+
 );
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
